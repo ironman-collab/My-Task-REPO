fce4e51d39c1a6291bf6affd0894f60b37aa6403
ironman-collab <uk6118660@gmail.com>
2024-11-04 13:41:33 +0500

    Input tasks

---
+++ source.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	//Task-1
+	int variable = 0, Variable2 = 0, Variable3 = 0, Variable4 = 0, table = 0, fuel = 0, rent = 0, bills = 0, Total = 0, chem = 0, math = 0, phy = 0,
+		num1 = 0, num2 = 0, num3 = 0;
+
+	cin >> variable;
+	cout << "Variable Value:" << variable << endl;
+	//Task-2
+
+	cout << "Enter variable value: ";
+	cin >> Variable2;
+	cout << "Variable value entered:" << Variable2 << endl;
+	//task-3
+	cout << "enter Variable Value:";
+	cin >> Variable3;
+	cout << "Enter Second Variable Value:";
+	cin >> Variable4;
+	cout << "Sum of Values = " << Variable3 + Variable4 << endl;
+	cout << "Differense of Values = " << Variable3 - Variable4 << endl;
+	cout << "Product of Values = " << Variable3 * Variable4 << endl;
+	cout << "Division of Values = " << Variable3 / Variable4 << endl;
+	//Task-4
+	cout << "Enter number for table =";
+	cin >> table; 
+	cout << table << " * 1 =" << table * 1 << endl;
+	cout << table << " * 2 =" << table * 2 << endl;
+	cout << table << " * 3 =" << table * 3 << endl;
+	cout << table << " * 4 =" << table * 4 << endl;
+	cout << table << " * 5 =" << table * 5 << endl;
+	cout << table << " * 6 =" << table * 6 << endl;
+	cout << table << " * 7 =" << table * 7 << endl;
+	cout << table << " * 8 =" << table * 8 << endl;
+	cout << table << " * 9 =" << table * 9 << endl;
+    cout << table << " * 10 =" << table * 10 << endl;
+	//Task-5
+	cout << "Enter the vclue of fuel =";
+	cin >> fuel;
+	cout << "Enter the value of rent =";
+	cin >> rent;
+	cout << "Enter the value of bills =";
+	cin >> bills;
+	cout << "Enter the value of Total =";
+	cin >> Total;
+	cout << "-------------------------------------------------------" << endl;
+	cout << "fuel:" << fuel << endl;
+	cout << "rent:" << rent << endl;
+	cout << "bills:" << bills << endl;
+	cout << "Total:" << Total << endl;
+	cout << "---------------------------------------------------------" <<endl;
+	//Task-6
+
+	cout << "Enter the vclue of Chemistry =";
+	cin >> chem;
+	cout << "Enter the value of Math =";
+	cin >> math;
+	cout << "Enter the value of Physucs =";
+	cin >> phy;
+	cout << "----------------------------------------------------------------------------------" << endl;
+
+
+
+	cout << "Chemistry:" << chem << "              Math;" << math << "                Physics;" << phy << endl;
+
+
+
+
+	cout << "------------------------------------------------------------------------------------"<<endl;
+	//Task-8
+	cout << "Please enter 3 numbers:";
+	cin >> num1; cin >> num2; cin >> num3; 
+	cout << "Reverse in this Order:" << num3 << "    " << num2 << "    " << num1 << "    " << endl;
+	//Task-9
+
+
+
+	return 0;
+}
